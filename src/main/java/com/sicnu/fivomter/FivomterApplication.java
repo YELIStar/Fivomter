0107c33e8bad27bdb4b18d7c6913d750703aad30
LOVELISTAR <HIJKLMOON@163.com>
2025-11-19 17:11:34 +0800

    Init object

---
+++ src/main/java/com/sicnu/fivomter/FivomterApplication.java
@@ -0,0 +1,13 @@
+package com.sicnu.fivomter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivomterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivomterApplication.class, args);
+	}
+
+}
